393eee75bd275f080e165a494e071095f02f207e
Peter Bingham <petersbingham@hotmail.co.uk>
2018-02-09 22:36:29 +0000

    Add completed tests for stelemConverge.

--- stelemPy/tests/test_stelemConverge.py
+++ stelemPy/tests/test_stelemConverge.py
@@ -21,12 +21,23 @@ class test_singleStable_oneStep(unittest.TestCase):
 class test_singleStable_twoStep(unittest.TestCase):
     def runTest(self):
         s = stelemConverger(SETS_1_IN, SETS_1_FOUT2)
-        self.assertEqual(s.createPoleSets(), SETS_1_COUT2)
+        cpoleSets = s.createPoleSets()
+        self.assertEqual(cpoleSets, SETS_1_COUT2)
+        s.writePriorRoots(cpoleSets)
+        self.assertEqual(cpoleSets, SETS_1_COUT2_PRE)
+        s.setClosestRootToLost(cpoleSets)
+        self.assertEqual(cpoleSets, SETS_1_COUT2_POST)
 
 class test_twoStable_oneLost_oneStep(unittest.TestCase):
     def runTest(self):
         s = stelemConverger(SETS_2_IN, SETS_2_FOUT1)
-        self.assertEqual(s.createPoleSets(), SETS_2_COUT1)
+        cpoleSets = s.createPoleSets()
+        self.assertEqual(cpoleSets, SETS_2_COUT1)
+        s.writePriorRoots(cpoleSets)
+        self.assertEqual(cpoleSets, SETS_2_COUT1_PRE)
+        s.setClosestRootToLost(cpoleSets)
+        self.assertEqual(cpoleSets, SETS_2_COUT1_POST)
+
 
 if __name__ == "__main__":
     #Just for debug
