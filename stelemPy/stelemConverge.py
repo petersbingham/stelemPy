f0cdce543c8c4c0089d76c72e365e8588b71850a
Peter Bingham <petersbingham@hotmail.co.uk>
2018-02-10 23:00:31 +0000

    Generalise the variable and function names used in the stelemConverge

--- stelemPy/stelemConverge.py
+++ stelemPy/stelemConverge.py
@@ -5,54 +5,54 @@ class stelemConverger:
         self.sets = sets
         self.stelementsResults = stelementsResults
 
-    def createPoleSets(self):
-        poleSets = []
+    def createConvergenceGroups(self):
+        convergenceGroups = []
         lastsi = None
         for si in range(len(self.stelementsResults)):
             stelementsResult = self.stelementsResults[si]
             for pi in range(len(stelementsResult[0])):
-                pole = stelementsResult[0][pi]
+                stelement = stelementsResult[0][pi]
                 hist = stelementsResult[1][pi]
                 lbl = stelementsResult[2][pi]
 
                 if lbl == "NEW":
-                    poleSets.append({si:[pole,hist,lbl]})
+                    convergenceGroups.append({si:[stelement,hist,lbl]})
                 elif lbl != "LOST":
                     found = False
-                    for i in range(len(poleSets)):
-                        if si not in poleSets[i] and poleSets[i][lastsi][1][0][1]==hist[1][1]:
-                            poleSets[i][si] = [pole,hist,lbl]
+                    for i in range(len(convergenceGroups)):
+                        if si not in convergenceGroups[i] and convergenceGroups[i][lastsi][1][0][1]==hist[1][1]:
+                            convergenceGroups[i][si] = [stelement,hist,lbl]
                             found = True
                             break
                     if not found:
-                        raise Exception("Could not find pole set for repeated pole!") #Should never be here
+                        raise Exception("Could not find stelement set for repeated stelement!") #Should never be here
                 else:
-                    for i in range(len(poleSets)):
-                        if poleSets[i][lastsi][0] == pole:
-                            poleSets[i][si] = [pole,None,"LOST"] #Just carry the value forward for now.
+                    for i in range(len(convergenceGroups)):
+                        if convergenceGroups[i][lastsi][0] == stelement:
+                            convergenceGroups[i][si] = [stelement,None,"LOST"] #Just carry the value forward for now.
 
             lastsi = si
-        return poleSets
+        return convergenceGroups
 
-    def writePriorRoots(self, poleSets):
-        for poleSet in poleSets:
-            firstFnd = min(poleSet.keys())
-            firstHist = poleSet[firstFnd][1]
+    def writePriorElements(self, convergenceGroups):
+        for stelementSet in convergenceGroups:
+            firstFnd = min(stelementSet.keys())
+            firstHist = stelementSet[firstFnd][1]
             for h in firstHist[1:]:
                 si = h[0]
                 ei = h[1]
-                poleSet[si] = [self.sets[si][ei],None,"PRE"]
+                stelementSet[si] = [self.sets[si][ei],None,"PRE"]
 
-    def setClosestRootToLost(self, poleSets):
-        for poleSet in poleSets:
-            for pole in poleSet:
-                if pole[2] == "LOST":
-                    si = pole[1][0][0]
+    def setClosestElementToLost(self, convergenceGroups):
+        for stelementSet in convergenceGroups:
+            for si,stelement in stelementSet.iteritems():
+                if stelement[2] == "LOST":
                     smallestDiff = None
-                    smallestIndex = None
+                    smallestei = None
                     for ei, el in enumerate(self.sets[si]):
-                        diff = num.absDiff(el, pole[0])
+                        diff = num.absDiff(el, stelement[0])
                         if smallestDiff is None or diff<smallestDiff:
                             smallestDiff = diff
-                            smallestIndex = ei
-                    pole[0] = self.sets[si][ei]
+                            smallestei = ei
+                    if smallestei is not None:
+                        stelement[0] = self.sets[si][smallestei]
--- stelemPy/tests/test_stelemConverge.py
+++ stelemPy/tests/test_stelemConverge.py
@@ -11,32 +11,32 @@ import unittest
 class test_singleStable_oneStep(unittest.TestCase):
     def runTest(self):
         s = stelemConverger(SETS_1_IN, SETS_1_FOUT1)
-        cpoleSets = s.createPoleSets()
-        self.assertEqual(cpoleSets, SETS_1_COUT1)
-        s.writePriorRoots(cpoleSets)
-        self.assertEqual(cpoleSets, SETS_1_COUT1_PRE)
-        s.setClosestRootToLost(cpoleSets)
-        self.assertEqual(cpoleSets, SETS_1_COUT1_POST)
+        convergenceGroups = s.createConvergenceGroups()
+        self.assertEqual(convergenceGroups, SETS_1_COUT1)
+        s.writePriorElements(convergenceGroups)
+        self.assertEqual(convergenceGroups, SETS_1_COUT1_PRE)
+        s.setClosestElementToLost(convergenceGroups)
+        self.assertEqual(convergenceGroups, SETS_1_COUT1_POST)
 
 class test_singleStable_twoStep(unittest.TestCase):
     def runTest(self):
         s = stelemConverger(SETS_1_IN, SETS_1_FOUT2)
-        cpoleSets = s.createPoleSets()
-        self.assertEqual(cpoleSets, SETS_1_COUT2)
-        s.writePriorRoots(cpoleSets)
-        self.assertEqual(cpoleSets, SETS_1_COUT2_PRE)
-        s.setClosestRootToLost(cpoleSets)
-        self.assertEqual(cpoleSets, SETS_1_COUT2_POST)
+        convergenceGroups = s.createConvergenceGroups()
+        self.assertEqual(convergenceGroups, SETS_1_COUT2)
+        s.writePriorElements(convergenceGroups)
+        self.assertEqual(convergenceGroups, SETS_1_COUT2_PRE)
+        s.setClosestElementToLost(convergenceGroups)
+        self.assertEqual(convergenceGroups, SETS_1_COUT2_POST)
 
 class test_twoStable_oneLost_oneStep(unittest.TestCase):
     def runTest(self):
         s = stelemConverger(SETS_2_IN, SETS_2_FOUT1)
-        cpoleSets = s.createPoleSets()
-        self.assertEqual(cpoleSets, SETS_2_COUT1)
-        s.writePriorRoots(cpoleSets)
-        self.assertEqual(cpoleSets, SETS_2_COUT1_PRE)
-        s.setClosestRootToLost(cpoleSets)
-        self.assertEqual(cpoleSets, SETS_2_COUT1_POST)
+        convergenceGroups = s.createConvergenceGroups()
+        self.assertEqual(convergenceGroups, SETS_2_COUT1)
+        s.writePriorElements(convergenceGroups)
+        self.assertEqual(convergenceGroups, SETS_2_COUT1_PRE)
+        s.setClosestElementToLost(convergenceGroups)
+        self.assertEqual(convergenceGroups, SETS_2_COUT1_POST)
 
 
 if __name__ == "__main__":
