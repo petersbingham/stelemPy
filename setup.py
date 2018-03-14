d759fddb443b9c4361a131b886a7c87fc191c311
Peter Bingham <petersbingham@hotmail.co.uk>
2018-03-14 20:02:28 +0000

    Move public functions to bottom of file.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='stelempy',
-      version='0.4',
+      version='0.5',
       description='Finds and quantifies stable elements over a number of sets',
       author="Peter Bingham",
       author_email="petersbingham@hotmail.co.uk",
