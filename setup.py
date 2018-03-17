50dc5a703b711e26f2f7b6da36e7a5ebfd4c880b
Peter Bingham <petersbingham@hotmail.co.uk>
2018-03-17 11:06:56 +0000

    Copy test file and README on installation.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+import shutil
+shutil.copy('README.md', 'stelempy/README.md')
 
 setup(name='stelempy',
-      version='0.5',
+      version='0.6',
       description='Finds and quantifies stable elements over a number of sets',
       author="Peter Bingham",
       author_email="petersbingham@hotmail.co.uk",
       packages=['stelempy'],
+      package_data={'stelempy': ['tests/*', 'README.md'],
      )
